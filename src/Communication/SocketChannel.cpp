65bcb34775a7b8699e16f42d09fb8d4be4fc681b
Smidt Jannik (VM/EMT3) <Jannik.Smidt@de.bosch.com>
2025-03-31 13:27:44 +0200

    Revert thread func

--- src/Communication/SocketChannel.cpp
+++ src/Communication/SocketChannel.cpp
@@ -274,7 +274,7 @@ public:
                 }
             }
 
-            Sleep(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
             if (timeoutInMilliseconds > 0) {
                 timeoutInMilliseconds--;
